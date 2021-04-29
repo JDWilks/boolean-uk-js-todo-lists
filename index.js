0d5de47f014fad746b11ca6a708a5c94ac9d84ce
Jonathon Wilks <jonathondwilks@icloud.com>
2021-04-29 16:12:49 +0100

    totally don't understand any of this at all - just saving as it gives me a break from looking at the code (smily face)

--- index.js
+++ index.js
@@ -7,3 +7,72 @@ and inspect the data
 console.log("users: ", users);
 
 console.log("todos: ", todos);
+
+
+// Create an alert that lists all users, with their ids, names and what city they're from
+
+for(var i = 0; i < users.length; i++){
+
+    console.log(name)
+    
+    }
+
+for(var i = 0; i < users.length; i++){
+
+    console.log([{name}])
+        
+    }    
+
+for(var i = 0; i < users.length; i++){
+
+    console.log({users.name})
+        
+    }  
+    
+for(var i = 0; i < users.length; i++){
+
+    console.log(users[]{.name})
+            
+    } 
+
+      
+     
+
+
+
+
+
+// - Prompt the user for a user id
+// - Display an alert with the username and all the todos titles that belong to that user 
+
+
+
+
+// yesterdays code
+
+let mainArray = [] 
+
+// creating outer array
+
+for(var i = 0; i < names.length; i++){
+
+    let tempArray = []
+
+    // creating temporary array to store each variable in
+
+    tempArray.push(uppercased[i]);
+
+    // pushing the first item in uppercased to tempArray
+
+    tempArray.push(names[i]);
+
+    // pushing the first item in names to tempArray
+
+    mainArray.push(tempArray);
+
+    // pushing tempArray into mainArray
+}
+
+console.log(mainArray)
+
+    // loggin the mainArray
